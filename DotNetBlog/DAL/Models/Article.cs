26177c580a036abb919ede0e50b3fa962c4fe1b6
Yaroslav <zherebetskiy00@gmail.com>
2019-01-08 21:32:27 +0200

    Initial commit

---
+++ DotNetBlog/DAL/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Models
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public ICollection<string> Tags { get; set; }
+    }
+}
---
+++ DotNetBlog/DAL/Models/Topic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
